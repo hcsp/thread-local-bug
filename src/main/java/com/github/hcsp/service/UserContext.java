52b331c1e94eaeccd2c41f58a1f9837bbf84cc7f
BirdSnail <39896910+BirdSnail@users.noreply.github.com>
2019-11-16 15:06:25 +0800

    修复ThreadLocal引起的串户的问题 (#2)
    修复ThreadLocal引起的串户的问题 (#2)

--- src/main/java/com/github/hcsp/service/UserContext.java
+++ src/main/java/com/github/hcsp/service/UserContext.java
@@ -13,6 +13,10 @@ public class UserContext {
     public static Optional<User> getCurrentUser() {
         return Optional.ofNullable(currentUser.get());
     }
+
+    public static void removeUser() {
+        currentUser.remove();
+    }
 //
 //
 //    public Optional<User> getCurrentUser() {
