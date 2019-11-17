605fc0923a7dc2d6b20ac1bbb8bf3488519372e3
yanghuadong <314726028@qq.com>
2019-11-17 14:16:15 +0800

    fix-thread-local-bug-2
    在处理相关请求之前清理下，删掉之前的无用代码

--- src/main/java/com/github/hcsp/controller/AuthController.java
+++ src/main/java/com/github/hcsp/controller/AuthController.java
@@ -45,7 +45,6 @@ public class AuthController {
     @ResponseBody
     public LoginResult logout() {
         SecurityContextHolder.clearContext();
-        UserContext.removeUser();
         return UserContext.getCurrentUser()
                 .map(user -> LoginResult.success("success", false))
                 .orElse(LoginResult.failure("用户没有登录"));
