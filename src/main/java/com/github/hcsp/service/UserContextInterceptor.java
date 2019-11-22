92ce512de56c1a45cdea2dd57331f929adb02f02
wg2049 <wg2049@hotmail.com>
2019-11-22 09:33:17 +0800

    实现了HandlerInterceptor接口的afterCompletion方法

--- src/main/java/com/github/hcsp/service/UserContextInterceptor.java
+++ src/main/java/com/github/hcsp/service/UserContextInterceptor.java
@@ -2,6 +2,7 @@ package com.github.hcsp.service;
 
 import com.github.hcsp.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -28,9 +29,12 @@ public class UserContextInterceptor implements HandlerInterceptor {
                 UserContext.setCurrentUser(user);
             }
         }
-
-        UserContext.removeCurrentUser();
         return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        UserContext.removeCurrentUser();
     }
 
 }
