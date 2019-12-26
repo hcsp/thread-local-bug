67b2457e5049c175d5b1df7d0b6479403ff2dade
kisslonglin <37239582+kisslonglin@users.noreply.github.com>
2019-12-26 16:39:22 +0800

    串号Bug修复 (#13)
    串号Bug修复 (#13)

--- src/main/java/com/github/hcsp/service/UserContextInterceptor.java
+++ src/main/java/com/github/hcsp/service/UserContextInterceptor.java
@@ -30,4 +30,9 @@ public class UserContextInterceptor implements HandlerInterceptor {
         }
         return true;
     }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserContext.removeCurrentUser();
+    }
 }
