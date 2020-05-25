da30423dbc7181539037333ad557a00ccc877a79
Suyu <zhuangsuyu1993@163.com>
2020-05-25 19:07:56 +0800

    Suyu (#27)
    Suyu (#27)

--- src/main/java/com/github/hcsp/controller/AuthController.java
+++ src/main/java/com/github/hcsp/controller/AuthController.java
@@ -45,9 +45,11 @@ public class AuthController {
     @ResponseBody
     public LoginResult logout() {
         SecurityContextHolder.clearContext();
-        return UserContext.getCurrentUser()
+        LoginResult loginResult = UserContext.getCurrentUser()
                 .map(user -> LoginResult.success("success", false))
                 .orElse(LoginResult.failure("用户没有登录"));
+        UserContext.remove();
+        return loginResult;
     }
 
     @PostMapping("/auth/register")
