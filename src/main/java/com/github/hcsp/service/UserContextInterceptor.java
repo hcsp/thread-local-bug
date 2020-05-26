96359322131bed5ad1de07b36a044ec91fe61ea5
1104049235 <1104049235@qq.com>
2020-05-26 11:12:17 +0800

    增加删除用户信息 (#28)
    增加删除用户信息 (#28)

--- src/main/java/com/github/hcsp/service/UserContextInterceptor.java
+++ src/main/java/com/github/hcsp/service/UserContextInterceptor.java
@@ -30,4 +30,9 @@ public class UserContextInterceptor implements HandlerInterceptor {
         }
         return true;
     }
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //删除user信息
+        UserContext.currentUser.remove();
+    }
 }
