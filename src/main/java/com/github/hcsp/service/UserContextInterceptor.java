fd67d7f3bc78cb1e2778bce12606ac6dc3f6629a
luhao <linesahao@outlook.com>
2021-11-30 11:38:12 +0800

    try

--- src/main/java/com/github/hcsp/service/UserContextInterceptor.java
+++ src/main/java/com/github/hcsp/service/UserContextInterceptor.java
@@ -34,6 +34,7 @@ public class UserContextInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        // set user info null
         UserContext.setCurrentUser(null);
     }
 }
